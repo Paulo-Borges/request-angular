10bbfe53629198c0db08e7472f2877aab468ea6e
Borges <borgespaulo72@yahoo.com.br>
2026-06-08 16:59:39 -0300

    feat: Enviroments criadas

--- src/app/cursos/cursos-lista/cursos-lista.ts
+++ src/app/cursos/cursos-lista/cursos-lista.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CursosService } from '../cursos.service';
+import { Curso } from './curso';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cursos-lista',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cursos-lista.html',
   styleUrl: './cursos-lista.scss',
 })
-export class CursosLista {}
+export class CursosLista implements OnInit {
+
+  cursos: Curso[] = [];
+
+  constructor(private service: CursosService){}
+
+  ngOnInit(): void {
+    this.service.list().subscribe(dados => this.cursos = dados);
+  }
+}
---
+++ src/app/cursos/cursos.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Curso } from './cursos-lista/curso';
+import { tap } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CursosService {
+
+  private readonly API = `${environment.API}cursos`;
+
+  constructor(private http: HttpClient){}
+
+
+  list() {
+    return this.http.get<Curso[]>(this.API)
+    .pipe(tap(console.log));
+  }
+}
